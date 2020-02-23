b657607d99e377cba1c4e7f5a050d8e3018038ff
jeffersonacm <jeffersonacm@hotmail.com>
2020-02-23 02:09:18 -0300

    Init project

---
+++ src/test/java/br/com/jefferson/salesmanegement/SalesmanegementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jefferson.salesmanegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesmanegementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
