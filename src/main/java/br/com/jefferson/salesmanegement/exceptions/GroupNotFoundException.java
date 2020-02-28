4dba92cfbf15a2097f59eb32468781ddaf5d2981
jeffersonacm <jeffersonacm@hotmail.com>
2020-02-27 21:56:58 -0300

    Improve exceptions

--- src/main/java/br/com/jefferson/salesmanegement/exceptions/GroupNotFoundException.java
+++ src/main/java/br/com/jefferson/salesmanegement/exceptions/GroupNotFoundException.java
@@ -3,12 +3,9 @@ package br.com.jefferson.salesmanegement.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "O grupo informado não existe")
 public class GroupNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public GroupNotFoundException() {
-        super("O grupo informado não existe");
-    }
 }
--- src/main/java/br/com/jefferson/salesmanegement/exceptions/UserEmailAlreadyUsedException.java
+++ src/main/java/br/com/jefferson/salesmanegement/exceptions/UserEmailAlreadyUsedException.java
@@ -1,11 +1,11 @@
 package br.com.jefferson.salesmanegement.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "O email informado não está disponível")
 public class UserEmailAlreadyUsedException extends RuntimeException {
 
     static final long serialVersionUID = 1L;
-
-    public UserEmailAlreadyUsedException() {
-        super("O email informado não está disponível");
-    }
 
 }
